3f9a7adf8748ee5f62203672d3fd15b0e0595e98
Leticia Simon <leticiasimon.photo@gmail.com>
2022-02-07 14:12:44 +0100

    Arquivos de pre-processamento

---
+++ Pre-Processamento/Pre-Proc1.R
@@ -0,0 +1,49 @@
+# Informações sobre a sessão
+sessionInfo()
+
+
+# Imprimir na tela
+print('Estou iniciando minha caminhada na carreira de Cientista de Dados')
+
+
+# Criar gráficos
+plot(1:25)
+
+
+# Instalar pacotes
+install.packages('randomForest')
+install.packages('ggplot2')
+install.packages("dplyr")
+install.packages("devtools")
+
+
+# Carregar o pacote
+library(ggplot2)
+
+
+# Descarregar o pacote
+detach(package:ggplot2)
+
+
+# Se souber o nome da função
+help(mean)
+?mean
+
+
+# Para buscar mais opções sobre uma função, use o pacote SOS
+install.packages("sos")
+library(sos)
+findFn("fread")
+
+
+# Se não souber o nome da função
+help.search('randomForest')
+help.search('matplot')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
+
+
+# Sair
+q()
+
